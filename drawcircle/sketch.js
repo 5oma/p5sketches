f46a9d0f82f13565b3ac3a84e1697b78b09ed843
user.email <ariel.carbajal.contacto@gmail.com>
2019-04-15 22:44:26 -0500

    Agregando drawcircle, un primer experimento con p5

---
+++ drawcircle/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+        ellipse(50, 50, 80, 80)
+}
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+	ellipse(50, 50, 80, 80)
+}
