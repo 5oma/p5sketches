51b74027162f38c72e424d90a27cb85bed61bb3a
user.email <ariel.carbajal.contacto@gmail.com>
2019-04-15 22:50:30 -0500

    Actualizando drawcircle.

--- drawcircle/sketch.js
+++ drawcircle/sketch.js
@@ -1,14 +1,12 @@
 function setup() {
-  // put setup code here
+  createCanvas(640, 480);
 }
 
 function draw() {
-        ellipse(50, 50, 80, 80)
-}
-function setup() {
-  // put setup code here
-}
-
-function draw() {
-	ellipse(50, 50, 80, 80)
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
