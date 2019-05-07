9c40a55ba785ff4918b6981927d86d4a81a9b215
5oma <ariel.carbajal.contacto@gmail.com>
2019-05-06 20:15:45 -0500

    De estrobo a smooth, ahora sí.

--- p5/moving-image/sketch.js
+++ p5/moving-image/sketch.js
@@ -1,5 +1,5 @@
 var maxCircleSize = 20
-var phase = 0, speed = 0.93;
+var phase = 0, speed = 0.03;
 
 function setup() {
   createCanvas(500, 500);
@@ -10,7 +10,7 @@ function draw() {
   var x = width/2; 
   var y = height/2 + sin(phase) * 50;
   phase = frameCount * speed;
-  var sizeOffset = (cos(phase) + 1) * 50;
+  var sizeOffset = (cos(phase) + 1) * .5;
   var circleSize = sizeOffset * maxCircleSize;
 
   ellipse(x, y, circleSize, circleSize);
