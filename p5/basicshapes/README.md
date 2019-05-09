0e8691be7f7e881646add03b500d3d9eaeb7e694
5oma <ariel.carbajal.contacto@gmail.com>
2019-05-08 21:22:11 -0500

    Añadiendo basicshapes.

---
+++ p5/basicshapes/README.md
@@ -0,0 +1,70 @@
+#Figuras básicas
+
+Lo más básico en p5.js. 
+
+P5.js es una librería en javaScript
+para dibujar con código.
+
+Primero, definimos nuestro espacio: 
+
+"""
+
+createCanvas(); 
+
+"""
+
+Si dejamos vacío el campo de parámetros, 
+se inicia en un lienzo de 100 x 100 pixeles. 
+
+
+Ubiquémonos en el centro. 
+
+"""js
+
+//Un punto en el centro. 
+point(50,50);
+
+"""
+
+Ahora, que ya sabemos un punto. 
+
+Usémoslo para definir otras figuras. 
+
+"""js
+
+line()
+
+triangle()
+
+quad()
+
+ellipse()
+
+"""
+
+Recuerda que al crear una figura, 
+si la quieres arriba, debe venir
+después en el orden del código.
+
+"""js
+
+//Aquí va el círculo arriba. 
+rect()
+ellipse()
+
+"""
+
+Así se aprende p5.js. 
+
+Bosquejando, es decir, delineando
+el código. 
+
+
+Después, cambio de estados.
+
+
+Viene el movimiento. 
+
+
+
+
---
+++ p5/basicshapes/sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+	//Crear un lienzo. 
+	createCanvas(480,120);
+
+	//Si la posición es la siguiente.
+	//00 es arriba a la derecha. 
+	//5050 es en el centro. 
+	//100100 es abajo a la izquierda,  
+
+	//Si quieres pantalla completa
+	//createCanvas(1920, 1080);
+}
+
+function draw() {
+        //Fondo
+	background(204);
+
+	//Orden del dibujo
+	//Lo que sigue se dibuja después
+	// Si quieres una figura arriba 
+	// de todas, tienes que 
+	//ponerla al último.
+
+
+
+
+	//Figuras básicas
+	
+	//linea(punto 1, punto 2)
+	//line(20, 50, 420, 110);
+
+
+	//triángulo(tres puntos)
+	//triangle(x1, y1, x2, y2, x3, y3);
+	//triangle(158, 55, 290, 91, 290, 112);	
+
+	//cuadrilátero
+	//quad(x1, y1, x2, y2, x3, y3, x4, y4);
+	//quad(158, 55, 199, 14, 392, 66, 351, 107);
+
+
+	//rectángulos
+	//rect(x, y, width, heigth
+	
+	//elipses
+	//ellipse(x, y, width, heigth)
+
+}
