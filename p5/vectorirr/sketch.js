ed57ba21b08889dcae1b78ed46ce825d07114755
5oma <ariel.carbajal.contacto@gmail.com>
2019-05-09 19:33:11 -0500

    Primer vector.

--- p5/vectorirr/sketch.js
+++ p5/vectorirr/sketch.js
@@ -1,9 +1,10 @@
 function setup() {
-	createCanvas();
+	createCanvas(200,200);
 }
 
 function draw() {
-	ellipse(5, 5, 8, 8);
-	line(5, 5, 5, 10);
+	ellipse(50, 50, 8, 8);
+	line(50, 50, 80, 50);
+	stroke(255);
 	ellipse(5, 10, 8, 8);
 }
